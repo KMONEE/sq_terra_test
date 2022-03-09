eed6313399a9ed8ae613cfedde03e384a3c1e489
Kevin <kevinlmerrell@gmail.com>
2022-03-09 13:34:44 -0500

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# sq_terra_test
- 
+# SubQuery Starter Package for Indexing Terra
+
+This is a starter project for Indexing Terra using this[https://github.com/DeveloperInProgress/subql] terra subql/node module.
+
+# Setup
+
+1. clone this repository
+
+`git clone https://github.com/DeveloperInProgress/subqlstarter-terra.git`
+
+2. install dependencies
+
+```
+cd subqlstarter-terra
+yarn install
+```
+
+3. After running the terra subql/node module once, build the project when the terra subql/node module asks for the missing mapping file in './dist/index.js'
+
+```
+yarn build
+```
+
+4.) Run the terra subql/node module once again and the indexing process will begin. 
---
+++ src/mappings/mappingHandlers.ts
@@ -0,0 +1,17 @@
+import {StarterEntity} from "../types";
+import { hashToHex } from '@terra-money/terra.js/dist/util';
+import { TerraEvent, TerraBlock } from '@subql/types-terra';
+
+export async function handleBlock(block: TerraBlock): Promise<void> {
+    let record = new StarterEntity(block.block.block.header.height);
+    record.hash = hashToHex(block.block.block_id.hash);
+    await record.save();
+}
+
+export async function handleEvent(event: TerraEvent): Promise<void> {
+    const record = await StarterEntity.get(event.block.block.header.height);
+    record.sender = event.event['transfer']['sender'];
+    record.recipient = event.event['transfer']['recipient'];
+    record.amount = event.event['transfer']['amount'];
+    await record.save();
+}
